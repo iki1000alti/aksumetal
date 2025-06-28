2824e4b68df701c7f6dac74600ed6e2f7826778e
iki1000alti <iki1000alti@gmail.com>
2025-06-28 10:20:01 +0300

    başarlı

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ app.use('/uploads', (req, res, next) => {
 
 // Cloudinary config
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'CLOUD_NAME',
-  api_key: process.env.CLOUDINARY_API_KEY || 'API_KEY',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'API_SECRET',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
