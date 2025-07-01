5796fa84833bcc14bb4d2f2c758b3200c8071f75
iki1000alti <iki1000alti@gmail.com>
2025-07-01 13:04:39 +0300

    ilk canlıya çıkış commit'i

--- index.js
+++ index.js
@@ -24,11 +24,8 @@ const PORT = process.env.PORT || 5001;
 
 // Sadece http ile çalışan domainler için CORS izni veriyoruz (SSL yok)
 const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:3000',
   'https://aksumetal.com',
-  'https://www.aksumetal.com',
-  'https://aksumetal.onrender.com',
+  'https://www.aksumetal.com'
 ];
 
 // CORS'u en üste al
